2f2234bd902d58432fa4626176ad9491c341ae85
PakkuDon <pakku.pd@gmail.com>
2015-11-13 23:44:55 +1100

    Implemented ConwayRules.isAliveInNextGeneration function

--- js/ConwayRules.js
+++ js/ConwayRules.js
@@ -1,5 +1,19 @@
 var ConwayRules = {
+    // Determines whether or not a cell is alive in the next phase
+    // given its current state and number of living neighbours
     isAliveInNextGeneration : function(isAlive, liveNeighbours) {
+        if (isAlive) {
+            if (liveNeighbours < 2 || liveNeighbours > 3) {
+                return false;
+            }
+            return true;
+        }
+        else {
+            if (liveNeighbours === 3) {
+                return true;
+            }
+            return false;
+        }
     }
 }
 
