9508a540c52f0dafbd0d49b532014b1508b2c061
PakkuDon <pakku.pd@gmail.com>
2015-11-24 00:56:49 +1100

    Implemented pause / play function
    - Added event handler for #toggle-running button.
    - Changed character used on pause button (previously double bars, now a
      black square).

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function() {
     // Input fields
     var txtWidth = document.querySelector('#width');
     var txtHeight = document.querySelector('#height');
+    var btnToggleState = document.querySelector('#toggle-running');
 
     var automaton = new Automaton(200, 240);
     var graphics = new Graphics(document.querySelector('canvas'));
@@ -11,9 +12,9 @@ window.addEventListener('load', function() {
     graphics.draw(automaton.getGrid());
 
     // Test draw method
-    setInterval(function() {
-        automaton.update();
-        graphics.draw(automaton.getGrid());
+    var intervalID;
+    intervalID = setInterval(function() {
+        update();
     }, delay);
 
     // Reset and set size of automaton on submit
@@ -26,8 +27,31 @@ window.addEventListener('load', function() {
         automaton.initialise(width, height);
     });
 
+    // Pause / Resume automaton updates
+    btnToggleState.addEventListener('click', function() {
+        // If not currently running, started updates and retain interval ID
+        if (typeof intervalID === 'undefined') {
+            intervalID = setInterval(function() {
+                update();
+            }, delay);
+            btnToggleState.innerHTML = '\u25FC';
+        }
+        // Else, clear interval
+        else {
+            clearInterval(intervalID);
+            intervalID = undefined;
+            btnToggleState.innerHTML = '\u25BA';
+        }
+    });
+
     // Reset grid state
     document.querySelector('#reset').addEventListener('click', function() {
         automaton.initialise(automaton.getWidth(), automaton.getHeight());
     });
+
+    // Create automaton's next generation 
+    function update() {
+        automaton.update();
+        graphics.draw(automaton.getGrid());
+    }
 });
